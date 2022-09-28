74f178df8d93f10e89997983d97338b26a69e2ed
Gustavo Morini <gustavommh@gmail.com>
2022-09-28 19:47:40 -0300

    formidable install and configure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var formidable = require("formidable");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/** POST upload page */
+router.post("/upload", (req, res, next) => {
+  const form = formidable({
+    uploadDir: "./upload",
+    keepExtensions: true,
+  });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.log(err);
+      next(err);
+      return;
+    }
+    res.json(files);
+  });
 });
 
 module.exports = router;
