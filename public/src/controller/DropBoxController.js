3f3224b1f4e945c89ef4858b3c67bc39f57446e3
Gustavo Morini <gustavommh@gmail.com>
2022-09-29 10:04:32 -0300

    file name, time left, toggle modal

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -4,6 +4,10 @@ class DropBoxController {
     this.inputFilesEl = document.querySelector("#files");
     this.snackModalEl = document.querySelector("#react-snackbar-root");
 
+    this.progressBarEl = this.snackModalEl.querySelector(".mc-progress-bar-fg");
+    this.nameFileEl = this.snackModalEl.querySelector(".filename");
+    this.timeLeftEl = this.snackModalEl.querySelector(".timeleft");
+
     this.initEvents();
   }
 
@@ -13,9 +17,18 @@ class DropBoxController {
     });
 
     this.inputFilesEl.addEventListener("change", (event) => {
+      // envia files para upload
       this.uploadTask(event.target.files);
-      this.snackModalEl.style.display = "block";
+      // abre modal
+      this.modalShow();
+      // zera o campo files
+      this.inputFilesEl.value = "";
     });
+  }
+
+  /** toggle modal */
+  modalShow(show = true) {
+    this.snackModalEl.style.display = show ? "block" : "none";
   }
 
   uploadTask(files) {
@@ -24,35 +37,51 @@ class DropBoxController {
     [...files].forEach((file) => {
       promises.push(
         new Promise((resolve, reject) => {
+          // formData
           let formData = new FormData();
           formData.append("input-file", file);
+          // instância do "ajax" XMLHttpRequest
+          const ajax = new XMLHttpRequest();
+          // abre a requisição
+          ajax.open("POST", "/upload");
 
-          // let ajax = new XMLHttpRequest();
-          // ajax.open("POST", "/upload");
-          // ajax.onload = (event) => {
-          //   try {
-          //     resolve(JSON.parse(ajax.responseText));
-          //   } catch (e) {
-          //     reject(e);
-          //   }
-          // };
-          // ajax.onerror = (event) => {
-          //   reject(event);
-          // };
-          // ajax.send(formData);
+          ajax.onload = (event) => {
+            // fecha modal
+            this.modalShow(false);
+            try {
+              resolve(JSON.parse(ajax.responseText));
+            } catch (e) {
+              reject(e);
+            }
+          };
+          // trataiva de erros
+          ajax.onerror = (event) => {
+            // fecha modal
+            this.modalShow(false);
+            reject(event);
+          };
+          // status upload arquivos
+          ajax.upload.onprogress = (event) => {
+            this.uploadProgress(event, file);
+          };
+          // hora atual inicio upload
+          this.startUploadTime = Date.now();
+          // envia os dados
+          ajax.send(formData);
 
           // fetch("/upload", {
           //   method: "POST",
           //   body: formData,
           // })
           //   .then((res) => {
+          //     console.log(res);
           //     res.json().then((data) => resolve(data));
           //   })
           //   .catch((e) => reject(e));
 
-          this.postFormData("/upload", formData)
-            .then((data) => resolve(data))
-            .catch((e) => reject(e));
+          // this.postFormData("/upload", formData)
+          //   .then((data) => resolve(data))
+          //   .catch((e) => reject(e));
         })
       );
     });
@@ -67,5 +96,44 @@ class DropBoxController {
       body: data,
     });
     return response.json();
+  }
+
+  /** tratativa para upload de arquivos */
+  uploadProgress(event, file) {
+    const { loaded, total } = event;
+    const { name } = file;
+    // porcentagem upload
+    const porcent = parseInt((loaded / total) * 100);
+    // tempo gasto upload
+    const timespent = Date.now() - this.startUploadTime;
+    // tempo restante
+    const timeleft = ((100 - porcent) * timespent) / porcent;
+    // barra progresso el
+    this.progressBarEl.style.width = `${porcent}%`;
+    // texto nome do arquivo el
+    this.nameFileEl.textContent = name;
+    // texto tempo restante el
+    this.timeLeftEl.textContent = this.formatTimeToHuman(timeleft);
+    // fecha modal e reinicia componentes
+  }
+
+  /** formata milesegundos de forma amigável */
+  formatTimeToHuman(duration) {
+    // converte milesegundos em segundos
+    const seconds = parseInt((duration / 1000) % 60);
+    // converte milesegundos em minutos
+    const minutes = parseInt((duration / (1000 * 60)) % 60);
+    // converte milesegundos em horas
+    const hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+    // determina qual msg será exibida
+    if (hours > 0) {
+      return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+    } else if (minutes > 0) {
+      return `${minutes} minutos e ${seconds} segundos`;
+    } else if (seconds > 0) {
+      return `${seconds} segundos`;
+    } else {
+      return "instantes";
+    }
   }
 }
